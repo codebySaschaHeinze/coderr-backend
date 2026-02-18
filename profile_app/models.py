42e5d4e74d5551afb9af2ac2779f91c402b3f7c0
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-18 14:52:50 +0100

    add Profile model

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile',
+    )
+
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    tel = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    working_hours = models.CharField(max_length=100, blank=True)
+    file = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Profile({self.user_id})'
