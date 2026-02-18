baaddfc72a510cde0b69a70103ef15a3f48d4d7b
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-18 19:41:45 +0100

    add OfferFilter

---
+++ offers_app/api/filters.py
@@ -0,0 +1,17 @@
+import django_filters 
+
+from offers_app.models import Offer
+
+
+class OfferFilter(django_filters.FilterSet):
+    creator_id = django_filters.NumberFilter(field_name='user_id')
+    min_price = django_filters.NumberFilter(field_name='min_price', lookup_expr='gte')
+    max_delivery_time = django_filters.NumberFilter(field_name='min_delivery_time', lookup_expr='lte')
+
+    class Meta:
+        model = Offer
+        fields = [
+            'creator_id', 
+            'min_price',
+            'max_delivery_time',
+        ]
