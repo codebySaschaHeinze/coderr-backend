8ef3357238f48801abc57ae0b9361e0fe1afb49d
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-18 13:48:12 +0100

    setup base routing

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('...'))
+    path('api/', include('auth_app.api.urls')),
+    path('api/', include('profile_app.api.urls')),
+    path('api/', include('offers_app.api.urls')),
+    path('api/', include('orders_app.api.urls')),
+    path('api/', include('reviews_app.api.urls')),
+    path('api/', include('baseinfo_app.api.urls')),
 ]
