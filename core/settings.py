af984d53d2f6d1fe4a0a237a4f5aa46f3ec3e94f
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-18 13:26:59 +0100

    update settings.py (Auth-Token)

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'django_filters',
 
@@ -125,6 +126,9 @@ STATIC_URL = 'static/'
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
