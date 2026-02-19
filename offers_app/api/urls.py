3841d34c93b83576942321dcfc763930e0fffe41
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-19 13:13:45 +0100

    add routing for offers, offer detail, offerdetails

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import OfferListCreateView
+from .views import OfferListCreateView, OfferDetailView, OfferDetailItemView
 
 
 urlpatterns = [
     path('offers/', OfferListCreateView.as_view(), name='offers'),
+    path('offers/<int:id>/', OfferDetailView.as_view(), name='offer-detail'),
+    path('offerdetails/<int:id>/', OfferDetailItemView.as_view(), name='offer-detail-item'),
 ]
