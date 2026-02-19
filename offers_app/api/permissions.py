b670a160bd015c6b68237f1dd8f60b4f9a677e43
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-19 13:14:19 +0100

    add business-only permissions

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -3,5 +3,15 @@ from rest_framework.permissions import BasePermission
 
 class IsBusinessUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.type == 'business')
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.type == 'business')
     
+
+class IsOfferOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            obj.user_id == request.user.id)
