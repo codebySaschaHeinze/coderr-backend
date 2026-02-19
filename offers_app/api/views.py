6a29a342a385fa40d52fb91be1fe6b1c5ceaaceb
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-19 13:24:33 +0100

    add OfferDetailView and OfferDetailtemView

--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,13 +1,14 @@
 from django.db.models import Min
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import generics, permissions
 
-from offers_app.models import Offer
+from offers_app.models import Offer, OfferDetail
 from .filters import OfferFilter
 from .pagination import OfferPagination
-from .permissions import IsBusinessUser
-from .serializers import OfferCreateSerializer, OfferListSerializer
+from .permissions import IsBusinessUser, IsOfferOwner
+from .serializers import OfferCreateSerializer, OfferDetailSerializer, OfferDetailViewSerializer, OfferListSerializer, OfferUpdateSerializer
 
 
 class OfferListCreateView(generics.ListCreateAPIView):
@@ -44,3 +45,35 @@ class OfferListCreateView(generics.ListCreateAPIView):
         context = super().get_serializer_context()
         context["request"] = self.request
         return context
+    
+
+class OfferDetailView(generics.RetrieveUpdateDestroyAPIView):
+    
+    lookup_url_kwarg = 'id'
+
+    def get_queryset(self):
+        return (
+            Offer.objects.select_related('user')
+            .prefetch_related('details')
+            .annotate(
+                min_price=Min('details__price'),
+                min_delivery_time=Min('details__delivery_time_in_days'),
+            )
+        )
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.IsAuthenticated()]
+        return [permissions.IsAuthenticated(), IsOfferOwner()]
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return OfferDetailViewSerializer
+        return OfferUpdateSerializer
+    
+
+class OfferDetailItemView(generics.RetrieveAPIView):
+    lookup_url_kwarg = 'id'
+    queryset = OfferDetail.objects.all()
+    serializer_class = OfferDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
