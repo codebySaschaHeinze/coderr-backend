a2915b30a479bc3b53ca49d393c469d1f576ea6a
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-19 20:41:55 +0100

    add Order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('in_progress', 'in_progress'),
+        ('completed', 'completed'),
+        ('cancelled', 'cancelled'),
+    )
+
+    customer_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders_as_customer',
+    )
+
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders_by_business',
+    )
+
+    title = models.CharField(max_length=250)
+    revisions = models.PositiveIntegerField()
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list, blank=True)
+    offer_type = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='in_progress')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order({self.id})'
+    
+    
