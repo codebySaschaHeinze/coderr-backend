d566a6242fe6298d00f0d9b884d92b555a0fed95
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-19 22:09:45 +0100

    add routing for orders and order-count

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,3 +1,15 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    OrdersView,
+    OrderDetailView,
+    OrderCountView,
+    CompletedOrderCountView
+)
+
+urlpatterns = [
+    path('orders/', OrdersView.as_view(), name='order'),
+    path('orders/<int:id>/', OrderDetailView.as_view(), name='order-detail'),
+    path('order-count/<int:business_user_id>/', OrderCountView.as_view(), name='order-count'),
+    path('completed-order-count/<int:business_user_id>', CompletedOrderCountView.as_view(), name='completed-order-count'),
+]
