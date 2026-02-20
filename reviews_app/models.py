c46b383db0a6e72a937ebdc32fd6fe09a45fc515
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-20 11:51:21 +0100

    add Review model

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='reviews_received',
+    )
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='reviews_written',
+    )
+    rating = models.PositiveSmallIntegerField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["business_user", "reviewer"],
+                name="unique_review_per_business_per_reviewer",
+            )
+        ]
+
+    def __str__(self):
+        return f'Review({self.id}) {self.rating}'
