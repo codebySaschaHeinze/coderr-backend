cff031a6e1802825ca1d48f6f7df15a4a6b25bac
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-23 16:37:46 +0100

    refactor (quotes/double quotes)

--- profile_app/api/urls.py
+++ profile_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProfileDetailView, BusinessProfilesListView, CustomerProfilesListView
 
 urlpatterns = [
-    path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
-    path("profiles/business/", BusinessProfilesListView.as_view(), name="profiles-business"),
-    path("profiles/customer/", CustomerProfilesListView.as_view(), name="profiles-customer"),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
+    path('profiles/business/', BusinessProfilesListView.as_view(), name='profiles-business'),
+    path('profiles/customer/', CustomerProfilesListView.as_view(), name='profiles-customer'),
 ]
