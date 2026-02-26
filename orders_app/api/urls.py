360fb7b8ed2dbee678d661e0f7b611620cbad5cb
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-02-26 16:57:22 +0100

    fix path "completed-order-count..."

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('orders/', OrdersView.as_view(), name='order'),
     path('orders/<int:id>/', OrderDetailView.as_view(), name='order-detail'),
     path('order-count/<int:business_user_id>/', OrderCountView.as_view(), name='order-count'),
-    path('completed-order-count/<int:business_user_id>', CompletedOrderCountView.as_view(), name='completed-order-count'),
+    path('completed-order-count/<int:business_user_id>/', CompletedOrderCountView.as_view(), name='completed-order-count'),
 ]
